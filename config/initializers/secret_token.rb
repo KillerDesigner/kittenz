ebd27c8f01b92a27f4356dac8a449fccf5aeb53e
Kyle Brett <kyle@kylebrett.com>
2013-10-10 11:24:47 -0700

    new railz project hooray

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kittenz::Application.config.secret_token = '8f9f373835d3183af2f742d3dc49ebe2a61b6a0ff5eee0102228e14425c55e2a58bbe92d7d17be11c6adb7dcae40f9755e851d437b288fcc16008d0852bde057'
